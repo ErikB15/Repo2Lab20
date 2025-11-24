877862486bf3c137c8d51dca32ffaec6abfa2137
Jim Ström <Jimbokalla@gmail.com>
2025-11-24 20:19:44 +0100

    Created Veterinarian creator

--- src/src/Veterinarian.java
+++ src/src/Veterinarian.java
@@ -1,2 +1,12 @@
 public class Veterinarian extends Employee{
+    private int amount;
+    private String name;
+    private String employeeID;
+
+
+    public Employee(int amount, String name, String employeeID){
+        this.amount = amount;
+        this.name   = name;
+        this.employeeID = employeeID;
+    }
 }
