cf9bce5c038e256c110f94eb2b20cb7b45d3126e
ErikB15 <erik.k.braun@gmail.com>
2025-11-24 20:36:22 +0100

    la till outprint 3

--- src/src/ZooManager.java
+++ src/src/ZooManager.java
@@ -6,6 +6,8 @@ public class ZooManager {
 
     public void initAnimals(int maxAnimals){
         System.out.println("Hello World");
+        System.out.println("Hello World2");
+        System.out.println("Hello World igen");
 
         animals = new Animal[maxAnimals];
     }
